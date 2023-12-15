ce08af4d0bc6e9aadbac05791ed75221647bc358
Andrzej Imiełowski <andrzej.imielowski@pwste.edu.pl>
2023-12-15 14:28:42 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NC6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
